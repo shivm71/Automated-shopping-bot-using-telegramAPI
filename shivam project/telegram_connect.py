1eda2282cec3163feffc2c5988425d7eb6b156e5
Shivam Shah <shivamshah71@gmail.com>
2020-04-21 09:35:15 +0000

    Delete telegram_connect.py

--- shivam project/telegram_connect.py
+++
@@ -1,7 +0,0 @@
-from telethon import TelegramClient,sync
-api_id = '1201180'
-api_hash = '1b3d4bdf22f0b52af3f1df6db89b62b7'
-phone = '+917828871116'
-username = 'tel_cloud'
-client = TelegramClient(username, api_id, api_hash)
-client.connect()
