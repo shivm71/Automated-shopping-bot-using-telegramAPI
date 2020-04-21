066574098c35580e896c59ec406b63b9748e1135
Shivam Shah <shivamshah71@gmail.com>
2020-04-21 09:34:28 +0000

    Replace run.py

--- shivam project/run.py
+++ shivam project/run.py
@@ -1,7 +1,12 @@
 import mongo_connect as mc
 import telegram_connect as tc
-import insert_user as iu
+from telegram_main import telegram
 col = mc.db["groups"]
-limit = 10
+limit = 20
+main = telegram(tc.client,col) # Telegram(client,column)
 for k in col.find():
-   tc.client.loop.run_until_complete(iu.main(k["group_id"],limit))
+   tc.client.loop.run_until_complete(main.insert_user_from_group(k["group_id"],limit)) # insert_user_from_group(self,group_id,limit)
+
+tc.client.loop.run_until_complete(main.insert_user_from_csv("user.csv","phone"))  # insert_user_from_csv(self,file_path,col_name)
+
+
