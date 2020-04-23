6b3a3192701190254b654181f5f591dada42b74a
Shivam Shah <shivamshah71@gmail.com>
2020-04-23 14:21:45 +0000

    major connection changes default client changed

--- src/telegram_connect.py
+++ src/telegram_connect.py
@@ -1,23 +1,40 @@
 from telethon import TelegramClient,sync
 import yaml
+import time
 
-num = 5  #update this number and you can get any session from the created session
+api_id = '1364667'
+api_hash = 'e1197805b1411dc174b7d813ba1901dc'
+phone = '+13105962190'
+session_name = 'tele1'
+filepath = 'client.yml'
+client = TelegramClient(session_name, api_id, api_hash)
 
-credentials = yaml.load(open('../config/application.yml'), Loader=yaml.FullLoader)
-api_id = credentials['tele'+str(num)]['api_id']
-api_hash = credentials['tele'+str(num)]['api_hash']
-phone = credentials['tele'+str(num)]['phone']
-username = credentials['tele'+str(num)]['username']
-
-client = TelegramClient(username, api_id, api_hash)
+with client:
+  pass  
 client.connect()
 
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    try:
-        client.sign_in(phone, input('Enter the code: '))
-    except Exception:
-        client.sign_in(password=input('Password: '))
+async def get_next_client(client,session_name,wait_time):
+    cred = yaml.load(open(filepath), Loader=yaml.FullLoader)
+    cred[session_name]["insert_time"] = int(time.time())
+    cred[session_name]["timeout"] = wait_time
+    with open(filepath, "w") as f:
+        yaml.dump(cred, f)
+    flag = True
+    while(flag):
+        time.sleep(10)
+        cred = yaml.load(open(filepath), Loader=yaml.FullLoader)
+        for key in cred:
+            if int(time.time()) - cred[key]["insert_time"] > cred[key]["timeout"]:
+                api_id =  cred[key]["api_id"]
+                api_hash = cred[key]["api_hash"]
+                session_name = key
+                flag = False
+                break
+            
+    client = TelegramClient(session_name, api_id, api_hash)
 
-me = client.get_me()
-print(me)
+    # with client:
+    #     pass  
+    await client.connect()
+    return (client,session_name)
+
