1efd05dc478f5b5dbac73f4f1123dc5b70167d8b
shivm71 <56828433+shivm71@users.noreply.github.com>
2020-05-12 12:13:20 +0530

    Update mongo_connect.py

--- src/mongo_connect.py
+++ src/mongo_connect.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
-mongoclient = MongoClient("mongodb://jarvis:datamantra890@127.0.0.1/crystal?retryWrites=true&w=majority")
+mongoclient = MongoClient("mongodb://jarvis:datamantra8890@127.0.0.1/crystal?retryWrites=true&w=majority")
 db = mongoclient['crystal']
