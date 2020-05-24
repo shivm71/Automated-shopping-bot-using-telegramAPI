f9cf00bfc799d85b8cbf2866992ac8206ebf0ca0
HarshitVarshney <hv.harshit@gmail.com>
2020-05-24 09:18:42 +0000

    adding tele13 session & removing *.session from gitignore. Try to not push any .session files unless its created as new

---
+++ src/database_operations.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from src import mongo_connect  # fetch db
+
+uname = mongo_connect.db["user_with_uname"]
+nouname = mongo_connect.db["user_without_uname"]
+
+
+def insert_user(client_name,user):
+    collection_name = uname
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    if not hasattr(user,'email'):
+        user.email = None
+    query = {
+        "telegram_id":user.id,
+        "telegram_username":user.username,
+        "userinfo":{
+            "firstname":user.first_name,
+            "lastname":user.last_name,
+            "email":user.email,
+            "gender":None,
+            "age":None,
+            "mobile_no":user.phone,
+            "pref_cat":[]
+        },
+        "created":date,
+        "updated":None,
+        "invite_status":False,
+        "membership_status":False,
+    }
+    if not (user.username or user.phone):
+        query["client_info"] = {client_name : user.access_hash}
+        collection_name = nouname
+    try:
+        collection_name.insert_one(query)
+        return True
+    except Exception as e:
+        print("duplicate entry level 1",e)
+        if collection_name == nouname:
+            return update_user_and_client_hash(client_name,user)
+        return True                                                                          # level 1
+
+
+def update_user_and_client_hash(client_name,user):
+    myquery = {"telegram_id":user.id}
+    pre = nouname.find_one(myquery)["client_info"]
+    pre[client_name] = user.access_hash
+    newvalues = { "$set": { "client_info":pre} }
+    if nouname.update_one(myquery, newvalues):                                           #level 2
+        return True
+    else:
+        print("not inserted at level 2")
+        return False
+
+def insert_group():
+    pass
+
+
+def update_invite_status(col,id):
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    myquery = {'_id':id}
+    newvalues = { "$set": { "invite_status": True,'updated':date } }
+    col.update_one(myquery, newvalues)
---
+++ src/insert_user.py
@@ -0,0 +1,33 @@
+import asyncio
+import threading
+from src import database_operations
+import telegram_connect
+
+
+def intercall(client_name,client,group_id):
+    loop.run_until_complete(insert(client_name,client,group_id))
+
+
+async def insert(client_name,client,group_id):
+    count = 0
+    entity = await client.get_entity(group_id)
+    #print(entity)
+    print(client_name)
+    try:
+        async for user in client.iter_participants(entity,limit=500):   #aggressive = True
+            if database_operations.insert_user(client_name,user):
+                count+=1
+            else:
+                print("Failed to insert after level 2")
+        print("inserted users for client=",client_name, " count=",count)
+    except Exception as e:
+        print("not working------",e)
+
+
+loop = asyncio.get_event_loop()
+client_dict,client_list = loop.run_until_complete(telegram_connect.get_all_client())
+
+for client_name in client_list:
+    x = threading.Thread(target= intercall, args=(client_name,client_dict[client_name],'campusdriveupdates'))
+    x.start()
+    x.join()
