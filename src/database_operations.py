756f2ab1a139b1f6b543c8f005bdcd97cda11f30
hvarshney <hvarshney@expedia.com>
2020-05-25 21:06:10 +0530

    changes for name_changer, send msg, limit offset, insert_user

--- src/database_operations.py
+++ src/database_operations.py
@@ -3,9 +3,10 @@ from src import mongo_connect  # fetch db
 
 uname = mongo_connect.db["user_with_uname"]
 nouname = mongo_connect.db["user_without_uname"]
+msg_col = mongo_connect.db["fetched_messages"]
 
 
-def insert_user(client_name,user):
+def insert_user(client_name,user,group_tag,loc_tags):
     collection_name = uname
     date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     if not hasattr(user,'email'):
@@ -22,6 +23,8 @@ def insert_user(client_name,user):
             "mobile_no":user.phone,
             "pref_cat":[]
         },
+        "group_tags" : group_tag,
+        "loc_tags" : loc_tags,
         "created":date,
         "updated":None,
         "invite_status":False,
@@ -34,10 +37,30 @@ def insert_user(client_name,user):
         collection_name.insert_one(query)
         return True
     except Exception as e:
+        update_tags(collection_name,user,group_tag,loc_tags)
         print("duplicate entry level 1",e)
         if collection_name == nouname:
             return update_user_and_client_hash(client_name,user)
         return True                                                                          # level 1
+
+
+def update_tags(collection_name,user,group_tag,loc_tags):
+    myquery = {"telegram_id":user.id}
+    res = collection_name.find_one(myquery)
+    loc = res["loc_tags"]
+    group = res["group_tags"]
+    loc+=loc_tags
+    group+=group_tag
+    loc = list(set(loc))
+    group = list(set(group))
+    newvalues = { "$set": { "group_tags":group,"loc_tags":loc} }
+    if collection_name.update_one(myquery, newvalues):                                       #level 2
+        return True
+    else:
+        print("not inserted at level 2")
+        return False
+
+    pass
 
 
 def update_user_and_client_hash(client_name,user):
@@ -45,10 +68,10 @@ def update_user_and_client_hash(client_name,user):
     pre = nouname.find_one(myquery)["client_info"]
     pre[client_name] = user.access_hash
     newvalues = { "$set": { "client_info":pre} }
-    if nouname.update_one(myquery, newvalues):                                           #level 2
+    if nouname.update_one(myquery, newvalues):                                           #level 3
         return True
     else:
-        print("not inserted at level 2")
+        print("not inserted at level 3")
         return False
 
 def insert_group():
@@ -60,3 +83,25 @@ def update_invite_status(col,id):
     myquery = {'_id':id}
     newvalues = { "$set": { "invite_status": True,'updated':date } }
     col.update_one(myquery, newvalues)
+
+
+def insert_message(obj,client_name):
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    query = {
+        'messsgae_id' :obj.id,
+        'message_content' : obj.message,
+        'client_used': client_name,
+        'self_id':obj.from_id,
+        'self_name':None,
+        'to_id':obj.to_id.user_id,
+        'to_name':None,
+        'time': date,
+        'reviewed': False,
+        'tags':[]
+    }
+    try:
+        msg_col.insert_one(query)
+        return True
+    except Exception as e:
+        print("insert msg error",e )
+        return False
--- src/get_update.py
+++ src/get_update.py
@@ -1,11 +1,46 @@
 from telethon import TelegramClient, events, sync 
 import asyncio
-import telegram_connect as tc 
+import telegram_connect as tc
+import src.database_operations as database_operations
 
-@tc.client.on(events.NewMessage)
+#@tc.client.on(events.NewMessage)
+# @tc.client.on(events.NewMessage)
+# async def main(event):
+#     print(event.message.stringify())
+#     obj = await tc.client.send_message(1139109151,event.message)    # to tele12 as backup
+#     if database_operations.insert_message(obj,tc.session_name):
+#         print('message arrived and inserted')
+#     else:
+#         print("error in inserting")
+
+# #calling method/can use asyncio too
+
+# tc.client.start()
+# tc.client.run_until_disconnected()
+
+##############
+api_hash= '81c1daf35a14dcfe8b20dc8fca1b679a'
+api_id= '1237102'
+username= 'tele13'
+
+client = TelegramClient(username, api_id, api_hash)
+client.connect()
+
+@client.on(events.NewMessage)
 async def main(event):
-    print(event.stringify()) #use event object to get data from various group 
-    await event.reply('hi!') 
+    print(event.message.id,"recieved")                               #in tele13
+    obj = await client.send_message(1139109151,event.message)
+    obj.from_id = (await client.get_me()).id
+    print(obj)
+    # to tele12
+    try:
+        if database_operations.insert_message(obj,"tele12"):
+            print('message arrived and inserted')
+        else:
+            print("error in inserting")
+    except Exception as e:
+        print(e)
+
 
 #calling method/can use asyncio too
 '''
